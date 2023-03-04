653b2473a7f59d6603bdf1d7e17110705a391434
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2023-03-04 07:45:48 +0530

    Update MaxSumofRectangleNoLargerThanK.cpp

--- PrefixSum_kadane/MaxSumofRectangleNoLargerThanK.cpp
+++ PrefixSum_kadane/MaxSumofRectangleNoLargerThanK.cpp
@@ -24,3 +24,35 @@ public:
         return res;
     }
 };
+
+// same approach java implementation
+class Solution {
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        //Write code here
+		int res=Integer.MIN_VALUE;
+		int row=matrix.length; int col=matrix[0].length;
+		for(int l=0;l<col;l++){
+			int[]sums=new int[row];
+			for(int r=l;r<col;r++){
+				for(int i=0;i<row;i++){
+					sums[i]+=matrix[i][r];
+				}
+				res=Math.max(res,maxSum(sums,k));
+			}
+		}
+		return res;
+    }
+	private int maxSum(int[]sums,int k){
+		TreeSet<Integer>bst=new TreeSet<>();
+		bst.add(0);
+		int ans=Integer.MIN_VALUE;
+		int curSum=0;
+		for(int sum:sums){
+			curSum+=sum;
+			Integer res=bst.ceiling(curSum-k);
+			if(res!=null) ans=Math.max(ans,curSum-res);
+			bst.add(curSum);
+		}
+		return ans;
+	}
+}

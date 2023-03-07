68354be2a215659d0580f1a94d0b8a99e0b19a23
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2023-03-08 00:12:00 +0530

    Create FindapairwithgiventargetinBST.java

---
+++ BST/FindapairwithgiventargetinBST.java
@@ -0,0 +1,3 @@
+//https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
+//https://practice.geeksforgeeks.org/problems/find-a-pair-with-given-target-in-bst/1
+//https://course.acciojob.com/idle?question=0dd4bc90-7cd3-45f8-bd73-81ada8c52c67

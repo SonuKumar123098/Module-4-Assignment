d4c05d318230a1e76bfdf1dd79a7cf78086d853b
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2023-03-08 10:51:51 +0530

    Create SearchinaBinarySearchTree.java

---
+++ BST/SearchinaBinarySearchTree.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+       while(root!=null){
+           if(root.val==val) return root;
+           else if(root.val>val) root=root.left;
+           else root=root.right;
+       }
+       return null;
+    }
+}
